39e529742ac1c1e1b1be1bee96d24dbcef16fb7e
jinhaEom <umjin98@naver.com>
2021-08-06 13:30:16 +0900

    kotlin

---
+++ Base/app/src/main/java/com/example/base/BaseActivity.kt
@@ -0,0 +1,40 @@
+package com.example.base
+
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
+abstract class BaseActivity : AppCompatActivity() {
+    abstract fun permissionGranted(RequestCode: Int)
+    abstract fun permissionDenied(RequestCode: Int)
+
+    fun requirePermissions(permissions: Array<String>, requestCode: Int) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            permissionGranted(requestCode)
+        } else {
+            val isAllPermissionsGranted = permissions.all {
+                checkSelfPermission(it) == PackageManager.PERMISSION_GRANTED
+
+            }
+            if(isAllPermissionsGranted){
+                permissionGranted(requestCode)
+            }else{
+                ActivityCompat.requestPermissions(this,permissions,requestCode)
+            }
+        }
+    }
+//사용자가 권한을 승인하거나 거부한 다음에 호출되는 메서드
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(grantResults.all{it==PackageManager.PERMISSION_GRANTED}){
+            permissionGranted(requestCode)
+        }else{
+            permissionDenied(requestCode)
+        }
+    }
+}
+
---
+++ Base/app/src/main/java/com/example/base/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.base
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import com.example.base.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity() {
+    val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnCamera.setOnClickListener {
+            requirePermissions(arrayOf(Manifest.permission.CAMERA),10)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==10){
+            if(resultCode== Activity.RESULT_OK){
+                Log.d("카메라","촬영성공!")
+            }else{
+                Log.d("카메라","촬영 실패!")
+            }
+        }
+    }
+
+    override fun permissionGranted(RequestCode: Int) {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, 99)
+    }
+    override fun permissionDenied(RequestCode: Int) {
+        Toast.makeText(baseContext,"권한 거부됨",Toast.LENGTH_LONG).show()
+    }
+}
