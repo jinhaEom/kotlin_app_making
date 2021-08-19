9fdcf7e5595e38095cb83ffabce103ad96cca697
jinhaEom <umjin98@naver.com>
2021-08-19 14:42:24 +0900

    kotlin

---
+++ SeoulPublicLibraries/app/src/main/java/bu/ac/kr/seoulpubliclibraries/SeoulOpenApi.kt
@@ -0,0 +1,18 @@
+package bu.ac.kr.seoulpubliclibraries
+
+import data.Library
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+class SeoulOpenApi {
+    companion object {
+        val DOMAIN = "http://openAPI.seoul.go.kr:8088/"
+        val API_KEY = "464b695052756d6a3738726b505a51"
+    }
+}
+
+interface SeoulOpenService {
+    @GET("{api_key}/json/SeoulPublicLibraryInfo/1/200")
+    fun getLibrary(@Path("api_key") key:String) : Call<Library>
+}
