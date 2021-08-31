e217bfc4fed9e92db1026c23aefb38d687f72ef8
jinhaEom <umjin98@naver.com>
2021-08-31 15:50:19 +0900

    kotlin

--- SmartPhone_Ranking/app/src/main/java/bu/ac/kr/smartphone_ranking/MainActivity.kt
+++ SmartPhone_Ranking/app/src/main/java/bu/ac/kr/smartphone_ranking/MainActivity.kt
@@ -1,24 +1,26 @@
 package bu.ac.kr.smartphone_ranking
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import bu.ac.kr.smartphone_ranking.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
-    var smartPhoneList = arrayListOf<SmartPhone>(
-        SmartPhone("1위", "갤럭시Z플립3", "galaxyzflip3"),
-        SmartPhone("2위", "갤럭시폴드3", "galaxyfold3")
-    )
+        var smartPhoneList = arrayListOf<SmartPhone>(
+
+            SmartPhone("1위","갤럭시Z플립3","galaxyzflip3.jpg")
+        )
+    var mainListView = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val smartphoneAdapter = ListViewAdapter(this, smartPhoneList)
-        ListViewAdapter.adapter = smartphoneAdapter
 
+        val listview = findViewById<ListView>(R.id.mainListView)
+       val smartPhoneAdapter = MainListAdapter(this,smartPhoneList)
+        listview.adapter = smartPhoneAdapter
 
     }
 }
---
+++ SmartPhone_Ranking/app/src/main/java/bu/ac/kr/smartphone_ranking/MainListAdapter.kt
@@ -0,0 +1,50 @@
+package bu.ac.kr.smartphone_ranking
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainListAdapter(val context: Context, val smartPhoneList: ArrayList<SmartPhone>) : BaseAdapter() {
+
+    override fun getItem(position: Int): Any {
+        return smartPhoneList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return smartPhoneList.size
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var convertView =convertView
+
+        if(convertView==null){
+            convertView = LayoutInflater.from(parent?.context).inflate(R.layout.item,parent,false)
+
+        }
+
+
+        val phoneImg = convertView!!.findViewById<ImageView>(R.id.phoneImg)
+        val phoneName = convertView!!.findViewById<TextView>(R.id.phoneName)
+        val phoneRanking = convertView!!.findViewById<TextView>(R.id.phoneRanking)
+
+        val phone = smartPhoneList[position]
+        val resourceId = context.resources.getIdentifier(phone.photo,"drawable",context.packageName)
+        phoneImg.setImageResource(resourceId)
+        phoneName.text= phone.name
+        phoneRanking.text=phone.ranking
+
+        return convertView!!
+    }
+
+
+
+
+}
